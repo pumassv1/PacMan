26c4452a8f69f891ab6653e64bfb71b4da35928c
pumassv1 <pumass775@gmail.com>
2021-02-17 15:10:27 +0100

    Added constants to game

---
+++ gameconstants.py
@@ -0,0 +1,11 @@
+import pygame
+
+### GAME CONSTANTS ###
+
+screen_width = 1024
+screen_height = 1024
+box_size = 32
+
+
+screen = pygame.display.set_mode((screen_width,screen_height)) # 200x200 blocks where each block is 4x4
+
--- gamemanager.py
+++ gamemanager.py
@@ -23,9 +23,9 @@ class GameManager:
                     self.board[y][x] = 'X'
 
     def draw_board(self):
-        blue_box = pygame.image.load('assets/box.png').convert()
+        blue_box = pygame.image.load('assets/box.png').convert_alpha()
         alpha_box = pygame.image.load('assets/alpha_box.png').convert_alpha()
-        bg_surface = pygame.image.load('assets/background.png').convert()
+        bg_surface = pygame.image.load('assets/background.png').convert_alpha()
         screen.blit(bg_surface, (0,0))
         for y in range(len(self.board)):
             for x in range(len(self.board)):
@@ -34,9 +34,3 @@ class GameManager:
                 else:
                     screen.blit(alpha_box,(y*box_size,x*box_size))
 
-
-
-x = GameManager()
-x.draw_board()
-pygame.display.update()
-input()

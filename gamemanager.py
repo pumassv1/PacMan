11765a4c03434df811eacef7e7c7179159dfef52
pumassv1 <pumass775@gmail.com>
2021-02-18 15:35:46 +0100

    gamemanager fix

--- gamemanager.py
+++ gamemanager.py
@@ -1,16 +1,6 @@
-import pygame
+from gameconstants import *
 
-pygame.init()
 
-### GAME CONSTANTS ###
-
-screen_width = 1024
-screen_height = 1024
-box_size = 32
-
-######################
-
-screen = pygame.display.set_mode((screen_width,screen_height)) # 200x200 blocks where each block is 4x4
 
 class GameManager:
     def __init__(self):

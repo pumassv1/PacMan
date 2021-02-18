397880fba2b93abb113f2b788e196e578fdf8519
pumassv1 <pumass775@gmail.com>
2021-02-18 15:36:24 +0100

    Added clock

--- gameconstants.py
+++ gameconstants.py
@@ -6,4 +6,8 @@ screen_width = 1024
 screen_height = 1024
 box_size = 32
 
+pygame.init()
+
 screen = pygame.display.set_mode((screen_width,screen_height)) # 200x200 blocks where each block is 4x4
+
+clock = pygame.time.Clock()
